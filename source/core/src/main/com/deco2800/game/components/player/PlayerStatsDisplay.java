4d0022bf3de8c2d1a8bc10e74529dced0c93904f
K0zzas17 <ochenje@gmail.com>
2021-08-25 20:10:14 +1000

    Added ProgressComponent to track horizontal progress

--- source/core/src/main/com/deco2800/game/components/player/PlayerStatsDisplay.java
+++ source/core/src/main/com/deco2800/game/components/player/PlayerStatsDisplay.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.deco2800.game.components.player;
 
 import com.badlogic.gdx.graphics.Texture;
@@ -7,9 +6,11 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.deco2800.game.components.CombatStatsComponent;
+import com.deco2800.game.components.ProgressComponent;
 import com.deco2800.game.components.SprintComponent;
 import com.deco2800.game.services.ServiceLocator;
 import com.deco2800.game.ui.UIComponent;
+
 
 /**
  * A ui component for displaying player stats, e.g. health.
@@ -17,9 +18,11 @@ import com.deco2800.game.ui.UIComponent;
 public class PlayerStatsDisplay extends UIComponent {
   Table table;
   Table table2;
+  Table table3;
   private Image heartImage;
   private Label healthLabel;
   private Label sprintLabel;
+  private Label progressLabel;
 
   /**
    * Creates reusable ui styles and adds actors to the stage.
@@ -30,6 +33,7 @@ public class PlayerStatsDisplay extends UIComponent {
     addActors();
     entity.getEvents().addListener("updateSprint", this::updateSprintLevelUI);
     entity.getEvents().addListener("updateHealth", this::updatePlayerHealthUI);
+    entity.getEvents().addListener("updateHealth2", this::updatePlayerProgressUI);
 
   }
 
@@ -43,6 +47,16 @@ public class PlayerStatsDisplay extends UIComponent {
     table.setFillParent(true);
     table.padTop(45f).padLeft(5f);
 
+    table2 = new Table();
+    table2.top().left();
+    table2.setFillParent(true);
+    table2.padTop(75f).padLeft(5f);
+
+    table3 = new Table();
+    table3.top().left();
+    table3.setFillParent(true);
+    table3.padTop(110f).padLeft(5f);
+
     // Heart image
     float heartSideLength = 30f;
     heartImage = new Image(ServiceLocator.getResourceService().getAsset("images/heart.png", Texture.class));
@@ -55,6 +69,9 @@ public class PlayerStatsDisplay extends UIComponent {
     CharSequence sprintText = String.format("Sprint: %d", sprint);
     sprintLabel = new Label(sprintText, skin, "large");
 
+    float position = entity.getComponent(ProgressComponent.class).getProgress();
+    CharSequence progressText = String.format("Progress: %.0f %%", position);
+    progressLabel = new Label(progressText, skin, "large");
 
     table.add(heartImage).size(heartSideLength).pad(5);
     table.add(healthLabel);
@@ -62,12 +79,13 @@ public class PlayerStatsDisplay extends UIComponent {
 
     //new table to add sprint label
     //not sure how to add sprint label below health label in same table
-    table2 = new Table();
-    table2.top().left();
-    table2.setFillParent(true);
-    table2.padTop(100f).padLeft(5f);
     table2.add(sprintLabel);
     stage.addActor(table2);
+
+    table3.add(progressLabel);
+    stage.addActor(table3);
+
+
   }
 
   @Override
@@ -93,115 +111,18 @@ public class PlayerStatsDisplay extends UIComponent {
     sprintLabel.setText(text);
   }
 
+  public void updatePlayerProgressUI(int position) {
+    CharSequence text = String.format("Health: %d", position);
+    progressLabel.setText(text);
+  }
+
   @Override
   public void dispose() {
     super.dispose();
     heartImage.remove();
     healthLabel.remove();
     sprintLabel.remove();
+    progressLabel.remove();
   }
 }
-=======
-package com.deco2800.game.components.player;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.deco2800.game.components.CombatStatsComponent;
-import com.deco2800.game.components.ProgressComponent;
-import com.deco2800.game.services.ServiceLocator;
-import com.deco2800.game.ui.UIComponent;
-
-/**
- * A ui component for displaying player stats, e.g. health.
- */
-public class PlayerStatsDisplay extends UIComponent {
-  Table table;
-  Table table2;
-  private Image heartImage;
-  private Label healthLabel;
-  private Label healthLabel2;
-
-  /**
-   * Creates reusable ui styles and adds actors to the stage.
-   */
-  @Override
-  public void create() {
-    super.create();
-    addActors();
-
-    entity.getEvents().addListener("updateHealth", this::updatePlayerHealthUI);
-    entity.getEvents().addListener("updateHealth2", this::updatePlayerHealthUI2);
-  }
-
-  /**
-   * Creates actors and positions them on the stage using a table.
-   * @see Table for positioning options
-   */
-  private void addActors() {
-    table = new Table();
-    table.top().left();
-    table.setFillParent(true);
-    table.padTop(45f).padLeft(5f);
-
-    table2 = new Table();
-    table2.top().left();
-    table2.setFillParent(true);
-    table2.padTop(75f).padLeft(5f);
-
-    // Heart image
-    float heartSideLength = 30f;
-    heartImage = new Image(ServiceLocator.getResourceService().getAsset("images/heart.png", Texture.class));
-
-    // Heart image2
-    //float heartSideLength2 = 30f;
-    //heartImage2 = new Image(ServiceLocator.getResourceService().getAsset("images/heart.png", Texture.class));
-
-    // Health text
-    int health = entity.getComponent(CombatStatsComponent.class).getHealth();
-    float position = entity.getComponent(ProgressComponent.class).getProgress();
-    CharSequence healthText = String.format("This is Health: %d", health);
-    CharSequence progressText = String.format("Progress: %.0f %%", position);
-    healthLabel = new Label(healthText, skin, "large");
-    healthLabel2 = new Label(progressText, skin, "large");
-
-
-
-    table.add(heartImage).size(heartSideLength).pad(5);
-    table.add(healthLabel).bottom();
-    //table2.add(heartImage2).size(heartSideLength2).pad(5);
-    table2.add(healthLabel2);
-    stage.addActor(table);
-    stage.addActor(table2);
-  }
-
-  @Override
-  public void draw(SpriteBatch batch)  {
-    // draw is handled by the stage
-  }
-
-  /**
-   * Updates the player's health on the ui.
-   * @param health player health
-   */
-  public void updatePlayerHealthUI(int health) {
-    CharSequence text = String.format("Health: %d", health);
-    healthLabel.setText(text);
-  }
-
-  public void updatePlayerHealthUI2(int health) {
-    CharSequence text = String.format("Health: %d", health);
-    healthLabel2.setText(text);
-}
-
-  @Override
-  public void dispose() {
-    super.dispose();
-    heartImage.remove();
-    healthLabel.remove();
-
-  }
-}
->>>>>>> progress_branch
--- source/core/src/main/com/deco2800/game/entities/factories/PlayerFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/PlayerFactory.java
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 package com.deco2800.game.entities.factories;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.deco2800.game.components.CombatStatsComponent;
+import com.deco2800.game.components.ProgressComponent;
 import com.deco2800.game.components.SprintComponent;
 import com.deco2800.game.components.npc.GhostAnimationController;
 import com.deco2800.game.components.player.InventoryComponent;
@@ -64,7 +64,7 @@ public class PlayerFactory {
             .addComponent(inputComponent)
             .addComponent(new PlayerStatsDisplay())
             .addComponent(new SprintComponent(100))
-
+            .addComponent(new ProgressComponent(stats.health))
             .addComponent(animator)
             .addComponent(new PlayerAnimationController());
 
@@ -79,65 +79,3 @@ public class PlayerFactory {
     throw new IllegalStateException("Instantiating static util class");
   }
 }
-=======
-package com.deco2800.game.entities.factories;
-
-import com.deco2800.game.components.CombatStatsComponent;
-import com.deco2800.game.components.player.InventoryComponent;
-import com.deco2800.game.components.player.PlayerActions;
-import com.deco2800.game.components.player.PlayerStatsDisplay;
-import com.deco2800.game.components.ProgressComponent;
-import com.deco2800.game.entities.Entity;
-import com.deco2800.game.entities.configs.PlayerConfig;
-import com.deco2800.game.files.FileLoader;
-import com.deco2800.game.input.InputComponent;
-import com.deco2800.game.physics.PhysicsLayer;
-import com.deco2800.game.physics.PhysicsUtils;
-import com.deco2800.game.physics.components.ColliderComponent;
-import com.deco2800.game.physics.components.HitboxComponent;
-import com.deco2800.game.physics.components.PhysicsComponent;
-import com.deco2800.game.rendering.TextureRenderComponent;
-import com.deco2800.game.services.ServiceLocator;
-
-/**
- * Factory to create a player entity.
- *
- * <p>Predefined player properties are loaded from a config stored as a json file and should have
- * the properties stores in 'PlayerConfig'.
- */
-public class PlayerFactory {
-  private static final PlayerConfig stats =
-      FileLoader.readClass(PlayerConfig.class, "configs/player.json");
-
-  /**
-   * Create a player entity.
-   * @return entity
-   */
-  public static Entity createPlayer() {
-    InputComponent inputComponent =
-        ServiceLocator.getInputService().getInputFactory().createForPlayer();
-
-    Entity player =
-        new Entity()
-            .addComponent(new TextureRenderComponent("images/box_boy_leaf.png"))
-            .addComponent(new PhysicsComponent())
-            .addComponent(new ColliderComponent())
-            .addComponent(new HitboxComponent().setLayer(PhysicsLayer.PLAYER))
-            .addComponent(new PlayerActions())
-            .addComponent(new CombatStatsComponent(stats.health, stats.baseAttack))
-            .addComponent(new ProgressComponent(stats.health))
-            .addComponent(new InventoryComponent(stats.gold))
-            .addComponent(inputComponent)
-            .addComponent(new PlayerStatsDisplay());
-
-    PhysicsUtils.setScaledCollider(player, 0.6f, 0.3f);
-    player.getComponent(ColliderComponent.class).setDensity(1.5f);
-    player.getComponent(TextureRenderComponent.class).scaleEntity();
-    return player;
-  }
-
-  private PlayerFactory() {
-    throw new IllegalStateException("Instantiating static util class");
-  }
-}
->>>>>>> progress_branch
