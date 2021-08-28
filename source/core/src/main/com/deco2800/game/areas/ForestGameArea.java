5d75a5fc833e78059764371e36a8372fc7198959
Carl <xzy1152241624@gmail.com>
2021-08-28 19:43:30 +1000

    Set up different types of static obstacles

--- source/core/src/main/com/deco2800/game/areas/ForestGameArea.java
+++ source/core/src/main/com/deco2800/game/areas/ForestGameArea.java
@@ -9,6 +9,7 @@ import com.deco2800.game.entities.Entity;
 import com.deco2800.game.entities.factories.NPCFactory;
 import com.deco2800.game.entities.factories.ObstacleFactory;
 import com.deco2800.game.entities.factories.PlayerFactory;
+import com.deco2800.game.services.GameTime;
 import com.deco2800.game.utils.math.GridPoint2Utils;
 import com.deco2800.game.utils.math.RandomUtils;
 import com.deco2800.game.services.ResourceService;
@@ -17,12 +18,19 @@ import com.deco2800.game.components.gamearea.GameAreaDisplay;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 /** Forest area for the demo game with trees, a player, and some enemies. */
 public class ForestGameArea extends GameArea {
   private static final Logger logger = LoggerFactory.getLogger(ForestGameArea.class);
-  private static final int NUM_TREES = 7;
+  private static final int NUM_TREES = 1;
+  private static final int NUM_BUILDINGS = 3;
+  private static final int NUM_ROCKS = 8;
+  private static final int NUM_PLANETS = 3;
+  private static final int NUM_ASTEROIDS = 5;
   private static final int NUM_GHOSTS = 2;
   private static final GridPoint2 PLAYER_SPAWN = new GridPoint2(0, 11);
+  private static final GridPoint2 PLATFORM_SPAWN = new GridPoint2(2,15);
   private static final float WALL_WIDTH = 0.1f;
   private static final String[] forestTextures = {
     "images/box_boy_leaf.png",
@@ -40,7 +48,20 @@ public class ForestGameArea extends GameArea {
     "images/iso_grass_3.png",
           "images/surface.png",
           "images/underground.png",
-          "images/sky.png"
+          "images/sky.png",
+          "images/rock1.png",
+          "images/rock2.png",
+          "images/rock3.png",
+          "images/rock4.png",
+          "images/asteroid.png",
+          "images/asteroid_2.png",
+          "images/platform1.png",
+          "images/platform2.png",
+          "images/platform3.png",
+          "images/platform4.png",
+          "images/platform5.png",
+          "images/building_1.png",
+          "images/planet1.png"
   };
   private static final String[] forestTextureAtlases = {
     "images/terrain_iso_grass.atlas", "images/ghost.atlas", "images/ghostKing.atlas", "images/boxBoy.atlas"
@@ -67,9 +88,13 @@ public class ForestGameArea extends GameArea {
 
     spawnTerrain();
     player = spawnPlayer();
-    spawnTrees();
-
-//    spawnGhosts();
+    spawnBuilding();
+    //spawnTrees();
+    spawnRocks();
+    spawnPlatform1();
+    spawnPlanet1();
+    //spawnAsteroids();
+    //spawnGhosts();
     //spawnGhostKing();
 
 //    playMusic();
@@ -117,7 +142,7 @@ public class ForestGameArea extends GameArea {
 
   private void spawnTrees() {
     //need to change it to the horizon view
-    GridPoint2 minPos = new GridPoint2(5, 10);
+    GridPoint2 minPos = new GridPoint2(2, 10);
     GridPoint2 maxPos = terrain.getMapBounds(0).sub(2, 20);
     for (int i = 0; i < NUM_TREES; i++) {
       GridPoint2 randomPos = RandomUtils.random(minPos, maxPos);
@@ -125,6 +150,74 @@ public class ForestGameArea extends GameArea {
       spawnEntityAt(tree, randomPos, true, false);
     }
   }
+
+  private void spawnPlanet1() {
+    GridPoint2 minPos = new GridPoint2(2, 20);
+    GridPoint2 maxPos = terrain.getMapBounds(0).sub(2, 5);
+    for (int i = 0; i < NUM_PLANETS; i++) {
+      GridPoint2 randomPos = RandomUtils.random(minPos, maxPos);
+      Entity planet1 = ObstacleFactory.createPlanet1();
+      spawnEntityAt(planet1, randomPos, true, false);
+    }
+  }
+
+
+  private void spawnBuilding() {
+    GridPoint2 minPos = new GridPoint2(2, 10);
+    GridPoint2 maxPos = terrain.getMapBounds(0).sub(2, 20);
+    for (int i = 0; i < NUM_BUILDINGS; i++) {
+      GridPoint2 randomPos = RandomUtils.random(minPos, maxPos);
+      Entity building = ObstacleFactory.createBuilding1();
+      spawnEntityAt(building, randomPos, true, false);
+    }
+  }
+
+  private void spawnRocks() {
+    GridPoint2 minPos = new GridPoint2(5, 10);
+    GridPoint2 maxPos = terrain.getMapBounds(0).sub(2, 20);
+    Random r = new Random();
+
+    for (int i = 0; i < NUM_ROCKS; i++) {
+      GridPoint2 randomPos = RandomUtils.random(minPos, maxPos);
+      Entity rock1 = ObstacleFactory.createRock1();
+      Entity rock2 = ObstacleFactory.createRock2();
+      Entity rock3 = ObstacleFactory.createRock3();
+      Entity rock4 = ObstacleFactory.createRock4();
+
+      if(r.nextInt(4) == 0) {
+        spawnEntityAt(rock1, randomPos, true, false);
+      } else if(r.nextInt(4) == 1) {
+        spawnEntityAt(rock2, randomPos, true, false);
+      } else if(r.nextInt(4) == 2) {
+        spawnEntityAt(rock3, randomPos, true, false);
+      } else {
+        spawnEntityAt(rock4, randomPos, true, false);
+      }
+    }
+  }
+
+  private void spawnPlatform1() {
+    Entity platform1 = ObstacleFactory.createPlatform1();
+    spawnEntityAt(platform1, PLATFORM_SPAWN, true, false);
+  }
+
+  /**private void spawnAsteroids() {
+    GridPoint2 minPos = new GridPoint2(2, 20);
+    GridPoint2 maxPos = terrain.getMapBounds(0).sub(2, 5);
+    Random r = new Random();
+
+    for (int i = 0; i < NUM_ASTEROIDS; i++) {
+      GridPoint2 randomPos = RandomUtils.random(minPos, maxPos);
+      Entity asteroid1 = ObstacleFactory.createAsteroid1();
+      Entity asteroid2 = ObstacleFactory.createAsteroid2();
+
+      if(r.nextInt(2) == 0) {
+        spawnEntityAt(asteroid1, randomPos, true, false);
+      } else {
+        spawnEntityAt(asteroid2, randomPos, true, false);
+      }
+    }
+  }*/
 
   private Entity spawnPlayer() {
     //need to change it to the horizon view
--- source/core/src/main/com/deco2800/game/entities/factories/ObstacleFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/ObstacleFactory.java
@@ -33,6 +33,188 @@ public class ObstacleFactory {
     return tree;
   }
 
+  public static Entity createRock1() {
+    Entity rock1 =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("images/rock1.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+
+    rock1.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    rock1.getComponent(TextureRenderComponent.class).scaleEntity();
+    rock1.scaleHeight(0.7f);
+    PhysicsUtils.setScaledCollider(rock1, 0.5f, 0.3f);
+    return rock1;
+  }
+
+  public static Entity createRock2() {
+    Entity rock2 =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("images/rock2.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+
+    rock2.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    rock2.getComponent(TextureRenderComponent.class).scaleEntity();
+    rock2.scaleHeight(0.8f);
+    PhysicsUtils.setScaledCollider(rock2, 0.5f, 0.3f);
+    return rock2;
+  }
+
+  public static Entity createRock3() {
+    Entity rock3 =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("images/rock3.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+
+    rock3.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    rock3.getComponent(TextureRenderComponent.class).scaleEntity();
+    rock3.scaleHeight(0.5f);
+    PhysicsUtils.setScaledCollider(rock3, 0.5f, 0.3f);
+    return rock3;
+  }
+
+  public static Entity createRock4() {
+    Entity rock4 =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("images/rock4.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+
+    rock4.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    rock4.getComponent(TextureRenderComponent.class).scaleEntity();
+    rock4.scaleHeight(0.9f);
+    PhysicsUtils.setScaledCollider(rock4, 0.5f, 0.3f);
+    return rock4;
+  }
+
+  public static Entity createPlanet1() {
+    Entity planet1 =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("images/planet1.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.NONE));
+
+    planet1.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    planet1.getComponent(TextureRenderComponent.class).scaleEntity();
+    planet1.scaleHeight(1.3f);
+    PhysicsUtils.setScaledCollider(planet1, 0.5f, 0.5f);
+    return planet1;
+  }
+
+  public static Entity createAsteroid1() {
+    Entity asteroid1 =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("images/asteroid.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.NONE));
+
+    asteroid1.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    asteroid1.getComponent(TextureRenderComponent.class).scaleEntity();
+    asteroid1.scaleHeight(1.2f);
+    PhysicsUtils.setScaledCollider(asteroid1, 0.5f, 0.5f);
+    return asteroid1;
+  }
+
+  public static Entity createAsteroid2() {
+    Entity asteroid2 =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("images/asteroid_2.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.NONE));
+
+    asteroid2.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    asteroid2.getComponent(TextureRenderComponent.class).scaleEntity();
+    asteroid2.scaleHeight(0.8f);
+    PhysicsUtils.setScaledCollider(asteroid2, 0.5f, 0.5f);
+    return asteroid2;
+  }
+
+  public static Entity createPlatform1() {
+    Entity platform1 =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("images/platform1.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+
+    platform1.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    platform1.getComponent(TextureRenderComponent.class).scaleEntity();
+    platform1.scaleHeight(0.5f);
+    PhysicsUtils.setScaledCollider(platform1, 0.5f, 0.3f);
+    return platform1;
+  }
+
+  public static Entity createPlatform2() {
+    Entity platform2 =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("images/platform2.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+
+    platform2.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    platform2.getComponent(TextureRenderComponent.class).scaleEntity();
+    platform2.scaleHeight(0.5f);
+    PhysicsUtils.setScaledCollider(platform2, 0.5f, 0.3f);
+    return platform2;
+  }
+
+  public static Entity createPlatform3() {
+    Entity platform3 =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("images/platform3.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+
+    platform3.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    platform3.getComponent(TextureRenderComponent.class).scaleEntity();
+    platform3.scaleHeight(0.5f);
+    PhysicsUtils.setScaledCollider(platform3, 0.5f, 0.3f);
+    return platform3;
+  }
+
+  public static Entity createPlatform4() {
+    Entity platform4 =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("images/platform4.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+
+    platform4.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    platform4.getComponent(TextureRenderComponent.class).scaleEntity();
+    platform4.scaleHeight(0.5f);
+    PhysicsUtils.setScaledCollider(platform4, 0.5f, 0.3f);
+    return platform4;
+  }
+
+  public static Entity createPlatform5() {
+    Entity platform5 =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("images/platform5.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+
+    platform5.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    platform5.getComponent(TextureRenderComponent.class).scaleEntity();
+    platform5.scaleHeight(0.5f);
+    PhysicsUtils.setScaledCollider(platform5, 0.5f, 0.3f);
+    return platform5;
+  }
+
+  public static Entity createBuilding1() {
+    Entity building1 =
+            new Entity()
+                    .addComponent(new TextureRenderComponent("images/building_1.png"))
+                    .addComponent(new PhysicsComponent())
+                    .addComponent(new ColliderComponent().setLayer(PhysicsLayer.NONE));
+
+    building1.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+    building1.getComponent(TextureRenderComponent.class).scaleEntity();
+    building1.scaleHeight(4f);
+    PhysicsUtils.setScaledCollider(building1, 0.5f, 0.5f);
+    return building1;
+  }
+
   /**
    * Creates an invisible physics wall.
    * @param width Wall width in world units
