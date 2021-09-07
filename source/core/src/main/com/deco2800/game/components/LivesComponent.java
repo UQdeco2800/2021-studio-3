2e44aac9b65300474c2c28cacd7272a735b2c2e8
K0zzas17 <ochenje@gmail.com>
2021-09-07 21:09:09 +1000

    Adding new LivesComponent class

---
+++ source/core/src/main/com/deco2800/game/components/LivesComponent.java
@@ -0,0 +1,45 @@
+package com.deco2800.game.components;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.deco2800.game.GdxGame;
+
+public class LivesComponent extends Component {
+
+    private int lives;
+    private GdxGame game;
+
+    public LivesComponent(int lives) {
+            this.lives = lives;
+        }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        //Will need to create a condition where lives can be added
+        //this.lives += lives;
+
+        if (entity != null) {
+            entity.getEvents().trigger("updateLives", lives);
+        }
+    }
+
+    public void addLives(int lives) {
+        this.lives+=lives;
+    }
+
+
+    public void livesRemaining() {
+        if (lives < 0) {
+
+        } else {
+            setLives(lives-1);
+        }
+    }
+
+
+
+
+}
