56f10b428529ec707598846635009aaea9b7c5a4
Justin Aavik <justinaavik@gmail.com>
2021-09-27 14:05:34 +1000

    Added the death png into the img loader and edited the death atlas file

--- source/core/src/main/com/deco2800/game/areas/ForestGameArea.java
+++ source/core/src/main/com/deco2800/game/areas/ForestGameArea.java
@@ -89,6 +89,7 @@ public class ForestGameArea extends GameArea {
           "images/JumpDamage(50-90).png",
           "images/JumpDamage(10-50).png",
           "images/IdleCharacters.png",
+          "images/Death.png",
           "images/0percent.png",
           "images/10percent.png",
           "images/20percent.png",
