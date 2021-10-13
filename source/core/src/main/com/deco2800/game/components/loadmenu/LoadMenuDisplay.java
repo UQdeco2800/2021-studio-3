634cfe0855b511a7b13c65ac9c363b5ecad7e223
K0zzas17 <ochenje@gmail.com>
2021-10-13 14:43:14 +1000

    implemented back button on load screen

--- source/core/src/main/com/deco2800/game/components/loadmenu/LoadMenuDisplay.java
+++ source/core/src/main/com/deco2800/game/components/loadmenu/LoadMenuDisplay.java
@@ -4,9 +4,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.deco2800.game.GdxGame;
 import com.deco2800.game.components.mainmenu.LoadingDisplay;
@@ -20,6 +23,8 @@ public class LoadMenuDisplay extends UIComponent {
     private static final float Z_INDEX = 2f;
     GdxGame game;
     Table loadTable;
+    Table buttonTable;
+    Table labelTable;
 
 
     public LoadMenuDisplay(GdxGame game) {
@@ -29,19 +34,46 @@ public class LoadMenuDisplay extends UIComponent {
     @Override
     public void create() {
         super.create();
+
+        entity.getEvents().addListener("back", this::back);
         addActors();
     }
 
-
     private void addActors() {
         loadTable = new Table();
+        buttonTable = new Table();
+        labelTable = new Table();
+
         loadTable.setFillParent(true);
+        buttonTable.setFillParent(true);
+        labelTable.setFillParent(true);
+        labelTable.center().top();
         loadTable.setBackground(new TextureRegionDrawable(
                 new Texture(Gdx.files.internal("images/loadScreen3.png"))));
-        /*Label title = new Label("Load Game", skin, "title");
-        loadTable.add(title);*/
+        Label title = new Label("Load Game", skin, "title");
+        labelTable.add(title);
 
+        TextButton backBtn = new TextButton("BACK", skin);
+        buttonTable.bottom().right();
+        buttonTable.add(backBtn).padBottom(15f).padRight(15f);;
+
+        backBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Back button clicked");
+                        entity.getEvents().trigger("back");
+                    }
+                });
+
+        Gdx.input.setInputProcessor(stage);
         stage.addActor(loadTable);
+        stage.addActor(buttonTable);
+        stage.addActor(labelTable);
+    }
+
+    public void back() {
+        game.setScreen(GdxGame.ScreenType.MAIN_MENU);
     }
 
     @Override
@@ -57,6 +89,7 @@ public class LoadMenuDisplay extends UIComponent {
     @Override
     public void dispose() {
         loadTable.clear();
+        buttonTable.clear();
         stage.dispose();
         super.dispose();
     }
