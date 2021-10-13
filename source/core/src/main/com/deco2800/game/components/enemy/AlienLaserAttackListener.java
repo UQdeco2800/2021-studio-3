2f54e2685b20b2bdd1ebf2a18fc830a8149e52ed
Shiyan Su <shiyan.su@uqconnect.edu.au>
2021-10-13 19:51:17 +1000

    implement speed chaning function to enemy's bullets and add two new enemies with basic functions

---
+++ source/core/src/main/com/deco2800/game/components/enemy/AlienLaserAttackListener.java
@@ -0,0 +1,25 @@
+package com.deco2800.game.components.enemy;
+
+import com.deco2800.game.areas.GameArea;
+import com.deco2800.game.components.Component;
+import com.deco2800.game.entities.Entity;
+import com.deco2800.game.entities.factories.EnemyFactory;
+
+public class AlienLaserAttackListener extends Component {
+    private Entity target;
+    private GameArea gameArea;
+
+    public AlienLaserAttackListener(Entity target, GameArea gameArea) {
+        this.target = target;
+        this.gameArea = gameArea;
+    }
+
+    public void create() {
+        super.create();
+        entity.getEvents().addListener("attack", this::attack);
+    }
+
+    void attack() {
+        EnemyFactory.createAlienLaserHoleWeapon(this.entity,target, gameArea);
+    }
+}
